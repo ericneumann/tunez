2dcd54980ea86ae2f9b31ead8e0ba22a5021b53a
Eric Neumann <ene1@hey.com>
2025-01-16 07:04:30 +0800

    formatting

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -1,5 +1,8 @@
 defmodule Tunez.Music.Album do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer
 
   postgres do
     table "albums"
