349cbca9d4dcd486dd4f29b17d5567664e5e8c80
Eric Neumann <ene1@hey.com>
2025-01-15 11:02:38 +0800

    reformat

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -1,5 +1,8 @@
 defmodule Tunez.Music.Artist do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer
 
   postgres do
     table "artists"
