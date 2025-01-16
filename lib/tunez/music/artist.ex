3cce7e407d7fd2eddcc313f64a11ac81e0c2c74d
Eric Neumann <ene1@hey.com>
2025-01-16 18:04:31 +0800

    artist previous names

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -35,6 +35,10 @@ defmodule Tunez.Music.Artist do
 
     attribute :biography, :string
 
+    attribute :previous_names, {:array, :string} do
+      default []
+    end
+
     create_timestamp :inserted_at
     update_timestamp :updated_at
   end
