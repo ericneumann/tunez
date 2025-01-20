d7619e3c78dd8097ec407501999def21cff24781
Eric Neumann <ene1@hey.com>
2025-01-20 09:28:16 +0800

    accounts user set role

--- lib/tunez/accounts.ex
+++ lib/tunez/accounts.ex
@@ -38,6 +38,11 @@ defmodule Tunez.Accounts do
 
   resources do
     resource Tunez.Accounts.Token
-    resource Tunez.Accounts.User
+
+    resource Tunez.Accounts.User do
+      define :set_user_role, action: :set_role, args: [:role]
+      define :get_user_by_id, action: :read, get_by: [:id]
+      define :get_user_by_email, action: :read, get_by: [:email]
+    end
   end
 end
